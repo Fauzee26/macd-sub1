dc0140933ef70e2b1024eb22992e239a09e7c4cc
Hilmy Fauzi <hilmy2602@gmail.com>
2019-04-28 17:54:06 +0800

    updatee

--- index.php
+++ index.php
@@ -1,3 +1,52 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=fauzistorages;AccountKey=u0iy9kBTdEd7bRhsTs1bIa3AQ0y29lf6h/YjLet0eJCrmVBeAVEuiS7ZPDOmrVHz8RkpOHmv41Jfcv5dBcslZA==;EndpointSuffix=core.windows.net";
+
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
+$host = "dicodingazuredb.database.windows.net";
+$user = "fauzee26";
+$pass = "!backpacker2602";
+        $db = "sub1dicodingfauzi";
+        try {
+            $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+            $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
+        }
+        if (isset($_POST['submit'])) {
+
+            $fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
+    $content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+    
+    $createContainerOptions = new CreateContainerOptions();
+    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+    // Set container metadata.
+    $createContainerOptions->addMetaData("key1", "value1");
+    $createContainerOptions->addMetaData("key2", "value2");
+
+      $containerName = "my_container";
+
+        // Create container.
+        $blobClient->createContainer($containerName, $createContainerOptions);
+
+       
+        //Upload blob
+        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+
+    $blobClient->createBlockBlob("my_container", $fileToUpload, $content);
+            }
+    ?>
 <html>
  <head>
  <Title>MACD- Sub1</Title>
@@ -28,22 +77,6 @@
     </div>
     <div class='table-container'>
     <?php
-
-    require_once 'vendor/autoload.php';
-require_once "./random_string.php";
-
-use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
-
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=fauzistorages;AccountKey=u0iy9kBTdEd7bRhsTs1bIa3AQ0y29lf6h/YjLet0eJCrmVBeAVEuiS7ZPDOmrVHz8RkpOHmv41Jfcv5dBcslZA==;EndpointSuffix=core.windows.net";
-
-// Create blob client.
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-
-
         $host = "dicodingazuredb.database.windows.net";
         $user = "fauzee26";
         $pass = "!backpacker2602";
@@ -55,27 +88,6 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
             echo "Failed: " . $e;
         }
         if (isset($_POST['submit'])) {
-
-            $fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
-    $content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-    
-    $createContainerOptions = new CreateContainerOptions();
-    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-
-    // Set container metadata.
-    $createContainerOptions->addMetaData("key1", "value1");
-    $createContainerOptions->addMetaData("key2", "value2");
-
-      $containerName = "my_container";
-
-        // Create container.
-        $blobClient->createContainer($containerName, $createContainerOptions);
-
-       
-        //Upload blob
-        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-
-    $blobClient->createBlockBlob("my_container", $fileToUpload, $content);
             try {
                 $name = $_POST['nama'];
                 $jurusan = $_POST['jurusan'];
