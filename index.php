de8b2d0c86d4edb1ef71c29edacea746c7206619
Fauzee26 <32213481+Fauzee26@users.noreply.github.com>
2019-04-29 10:34:55 +0800

    fixed analyze image

--- index.php
+++ index.php
@@ -7,38 +7,13 @@ use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+?>
 
-// $connectionString = "DefaultEndpointsProtocol=https;AccountName=fauzistorages;AccountKey=u0iy9kBTdEd7bRhsTs1bIa3AQ0y29lf6h/YjLet0eJCrmVBeAVEuiS7ZPDOmrVHz8RkpOHmv41Jfcv5dBcslZA==;EndpointSuffix=core.windows.net";
 
-// // Create blob client.
-// $blobClient = BlobRestProxy::createBlobService($connectionString);
-
-//         if (isset($_POST['submit'])) {
-
-//     //         $fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
-//     // $content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-    
-//     // $createContainerOptions = new CreateContainerOptions();
-//     // $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-
-//     // // Set container metadata.
-//     // $createContainerOptions->addMetaData("key1", "value1");
-//     // $createContainerOptions->addMetaData("key2", "value2");
-
-//     //   $containerName = "my_container";
-
-//     //     // Create container.
-//     //     $blobClient->createContainer($containerName, $createContainerOptions);
-       
-//     //     //Upload blob
-//     //     $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-
-//     // $blobClient->createBlockBlob("my_container", $fileToUpload, $content);
-//             }
-    ?>
 <html>
  <head>
  <Title>MACD-Sub1</Title>
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
  </head>
  <body>
  <div>
@@ -190,5 +165,79 @@ $fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
     </div>
     <br style='clear: both'/>
  </div>
+
+ <script type="text/javascript">
+    function processImage() {
+        
+        var subscriptionKey = "4b145aca423d417594852790c1d1aa79";
+ 
+        var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+        var sourceImageUrl = document.getElementById("inputImage").value;
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+</script>
+
+<h1>Analyze image:</h1>
+Enter the URL to an image, then click the <strong>Analyze image</strong> button.
+<br><br>
+Image to analyze:
+<input type="text" name="inputImage" id="inputImage"
+    value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
+<button onclick="processImage()">Analyze image</button>
+<br><br>
+<div id="wrapper" style="width:1020px; display:table;">
+    <div id="jsonOutput" style="width:600px; display:table-cell;">
+        Response:
+        <br><br>
+        <textarea id="responseTextArea" class="UIInput"
+                  style="width:580px; height:400px;"></textarea>
+    </div>
+    <div id="imageDiv" style="width:420px; display:table-cell;">
+        Source image:
+        <br><br>
+        <img id="sourceImage" width="400" />
+    </div>
+</div>
  </body>
  </html>
